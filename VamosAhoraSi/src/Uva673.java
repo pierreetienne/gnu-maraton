bf3bdf613ac45f7ed90158fd1fbc6146a0accd95
pierreetienne <etienne.pradere@gmail.com>
2012-06-17 00:01:01 -0500

    673 no ha pasado

--- VamosAhoraSi/src/Uva673.java
+++ VamosAhoraSi/src/Uva673.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
@@ -10,64 +8,42 @@ public class Uva673 {
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(bf.readLine());
+		int N = Integer.parseInt(bf.readLine().trim());
 		StringBuilder sb = new StringBuilder();
 		while(N -- > 0 ){
 			m = bf.readLine().trim().toCharArray();
-			if(m[0] == ')' || m[0]== ']' || m[m.length-1]=='(' || m[m.length-1]=='[')
+			if(m.length==0)
+				sb.append("Yes\n");
+			else if( m[0] == ')' || m[0]== ']' || m[m.length-1]=='(' || m[m.length-1]=='[' || m.length%2 ==1)
 				sb.append("No\n");
 			else {
-				int contadorP = 0 , contadorQ = 0;
-				int[][] cantidad = new int[m.length][2];
-				if(m[0] == '(')
-					cantidad[0][0]=1;
-				if(m[0]=='[')
-					cantidad[0][1]=1;
-				
-				for(int i=1;i<m.length;++i){
-					if(m[i]=='('){
-						cantidad[i][0]+= cantidad[i-1][0]+1;
-						cantidad[i][1] += cantidad[i-1][1];
-					}
-					else if(m[i]=='['){
-						cantidad[i][1]  += cantidad[i-1][1]+1;
-						cantidad[i][0] += cantidad[i-1][0];
-					}
-					else if(m[i]==']'){
-						cantidad[i][1]+= cantidad[i-1][1]-1;
-						cantidad[i][0] += cantidad[i-1][0];
-					}
-					else if(m[i]==')'){
-						cantidad[i][0] += cantidad[i-1][0]-1;
-						cantidad[i][1] += cantidad[i-1][1];
-					}
-				}
-				System.out.println();
-				
-				for(int i=0;i<m.length;++i){
-					System.out.println(cantidad[i][0] + " -- " + cantidad[i][1] + " >> "  + m[i]);
-				}
-				System.out.println();
-				for(int i=0;i<m.length;++i){
-					if(m[i]=='(')
-						contadorP++;
-					else if(m[i]==')')
-						contadorP--;
-					else if(m[i]=='[')
-						contadorQ ++;
-					else if(m[i]==']')
-						contadorQ--;
-					if(contadorP < 0 || contadorQ < 0 )
-						break;
-					System.out.println("i: " + i + " ( " + contadorP +" , " + contadorQ +" )");
-				}
-				if(contadorP == 0 && contadorQ == 0)
-					sb.append("Yes\n");
-				else 
-					sb.append("No\n");
+				sb.append((valido( 0))?"Yes\n":"No\n");
 			}
 		}
 		System.out.print(new String(sb));
 	}
 	
+	
+	static boolean valido( int desde) {
+		char inicial = m[desde];
+		if(inicial==']'||inicial==')')return false;
+		char fin = (inicial=='(')?')':']';
+		char inicialB = inicial=='('?'[':'(';
+		char finB = fin==']'?')':']';
+		int contA=1;
+		int contB=0;
+		for(int i=desde+1;i<m.length&&contA!=0&&contB!=-1;++i){
+			if(m[i]==fin)
+				contA--;
+			else if(m[i]==inicial)
+				contA++;
+			else{
+				if(!valido( i))
+					return false;
+			}
+				
+			
+		}
+		return (contB==-1)?false:true;
+	}
 }
