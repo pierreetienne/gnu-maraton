860da7f0dbd14a234896c4eae3c9340d2db5b8df
pierreetienne <etienne.pradere@gmail.com>
2012-07-26 22:45:01 -0500

    k

--- VamosAhoraSi/src/knigs.java
+++ VamosAhoraSi/src/knigs.java
@@ -1,7 +1,4 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.PriorityQueue;
@@ -24,61 +21,48 @@ public class knigs {
 			st = new StringTokenizer(bf.readLine());
 			int N = n + k - 2;
 			int[] alce = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
-			Comparator<Integer> c = new Comparator<Integer>() {
-				public int compare(Integer o1, Integer o2) {
-					return o2 - o1;
+			
+			PriorityQueue<Integer> cola = new PriorityQueue<Integer>();
+			
+			PriorityQueue<int[]> l = new PriorityQueue<int[]>(1, new Comparator<int[]>() {
+				public int compare(int[] o1, int[] o2) {
+					return o2[0]-o1[0];
 				}
-			};
-			PriorityQueue<Integer>[] colas = new PriorityQueue[n];
-			for(int i=0;i<colas.length;++i)
-				colas[i]=new PriorityQueue<Integer>(1,c);
-
-			colas[alce[0]-2011].add(alce[1]);
-
+			});
+			
+		
+			int anio = 0;
+			
 			for(int i=0;i<N;++i){
 				st = new StringTokenizer(bf.readLine());
-				int a = Integer.parseInt(st.nextToken());
-				int p = Integer.parseInt(st.nextToken());
-				colas[a-2011].add(p);
-			}
-
-
-			boolean encontre = false;
-			int anio  = 0 ;
-			int val = colas[0].poll();
-			if(val == alce[1]){
-				encontre = true;
-				anio = 2011;
-			}
-
-			while(!encontre){
-				int cont=0 , posicionGanador = -1, maxAnio=0;
-				for(int i=0;i<colas.length&& cont<k;++i){
-					cont += colas[i].size();
-					if(cont>0  && posicionGanador == -1)
-						posicionGanador = i;
-					else if(posicionGanador != -1 && !colas[posicionGanador].isEmpty() &&!colas[i].isEmpty()&&colas[i].peek() > colas[posicionGanador].peek())
-						posicionGanador = i;
-					if(i+2011 > maxAnio && colas[i].size()>0 )
-						maxAnio = i+2011;
-				}
-				//System.out.println("posicion Ganador " + posicionGanador +  " cont " + cont);
-				if(posicionGanador != -1 && cont==k)
-				{
-					val = colas[posicionGanador].poll();
-					if(val == alce[1]){
-						encontre = true;
-						anio = maxAnio;
-					}
-				}
+				int[] val = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+				if(cola.size()<k && val[0]==2011)
+					cola.add(val[1]);
 				else
-					break;
+					l.add(val);
 			}
-
+		
+			if(alce[0]==2011)
+				cola.add(alce[1]);
+			else
+				l.add(alce);
+			
+			boolean encontre = false;
+			while(!l.isEmpty() && !encontre){
+				int peso =  cola.poll();
+				if(peso == alce[1])
+					encontre=true;
+				else{
+					int[] x  = l.poll();
+					anio = x[0];
+					cola.add(x[1]);
+				}
+			}
 			if(encontre)
 				sb.append(anio+"\n");
-			else 
+			else
 				sb.append("unknown\n");
+		
 		}
 		System.out.print(new String(sb));
 	}
