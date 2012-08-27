3b9ff7669e1585192619f21a0974b254919b8384
Pierre Etienne <etienne.pradere@gmail.com>
2012-08-27 17:15:13 -0500

    s

--- VamosAhoraSi/src/Uva10298.java
+++ VamosAhoraSi/src/Uva10298.java
@@ -9,7 +9,20 @@ public class Uva10298 {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		String linea = bf.readLine().trim();
 		while(!linea.equals(".")){
+			for(int i=0;i<linea.length();i++)
+			{
+				boolean encontre = true;
+				for(int j=0;j<linea.length()&& i+j<linea.length()&&encontre;++j){
+					if(linea.charAt(j)!= linea.charAt(i+j))
+						encontre=false;
+				}
+				if(encontre)
+					System.out.println("aa " + i);
+			}
+			linea = bf.readLine().trim();
+			
 		}
 	}
+	
 
 }
