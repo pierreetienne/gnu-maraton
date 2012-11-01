f4d1fbe7b7612c023c006cd2f219a422ea6e4dfd
Pierre Etienne <etienne.pradere@gmail.com>
2012-11-01 17:36:43 -0500

    n

--- VamosAhoraSi/src/Uva12516.java
+++ VamosAhoraSi/src/Uva12516.java
@@ -1,14 +1,28 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.StringTokenizer;
-
 
 public class Uva12516 {
 
-	/**
-	 * @param args
-	 */
+
+	static class No{
+		int num;
+		char posi;
+		public String toString(){
+			return "Num: "+ num +" Posi : " +posi; 
+		}
+	}
+	
+	static final Comparator<No> comparator =  new Comparator<No>() {
+		public int compare(No o1, No o2) {
+			return o1.num-o2.num;
+		}
+	};
+	
 	public static void main(String[] args) throws Exception
 	{
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
@@ -18,39 +32,84 @@ public class Uva12516 {
 			int F = Integer.parseInt(st.nextToken()), C = Integer.parseInt(st.nextToken());
 			if(F==0 && C == 0) break;
 			int N = Integer.parseInt(bf.readLine().trim());
-			int[][] m = new int[F][C+1];
-			int[][] cpy = new int[F][C+1];
-			for(int i=0;i<m.length;++i){Arrays.fill(m[i],-1); Arrays.fill(cpy[i], -1);}
+			HashMap<Character, ArrayList<No>> data = new HashMap<Character, ArrayList<No>>();
 			for(int i=0;i<N;++i){
 				st = new StringTokenizer(bf.readLine());
 				String x = st.nextToken();
-				int posx =  Character.toUpperCase(x.charAt(0))-'A';
+				if(data.get(x.charAt(0))==null)
+					data.put(x.charAt(0),new ArrayList<No>());
 				int posy = Integer.parseInt(x.substring(1))-1;
+				String aux = x;
 				x = st.nextToken();
-				if(x.trim().equals("-"))
-					cpy[posx][posy]=m[posx][posy]=-2;
-				else
-					cpy[posx][posy+1]=m[posx][posy+1]=-2;
+				No p = new No();
+				p.num = posy;
+				p.posi = x.charAt(0);
+				data.get(aux.charAt(0)).add(p);				
 			}
 			N = Integer.parseInt(bf.readLine().trim());
-			boolean funciona = true;
+			
 			for(int i=0;i<N;++i){
 				String x = bf.readLine().trim();
-				int posx =  Character.toUpperCase(x.charAt(0))-'A';
 				int posy = Integer.parseInt(x.substring(1))-1;
-				if(m[posx][posy]==-1 && m[posx][posy+1]==-1)
-					m[posx][posy]=m[posx][posy+1]=posy;
-				else if(m[posx][posy+1]==-1&&m[posx][posy]==-2 )
-					m[posx][posy+1]=-2;
-				else if(m[posx][posy]==-1&&m[posx][posy+1]==-2 )
-					m[posx][posy]=-2;
-				else if(m[posx][posy]>=0&& m[posx][posy+1]==-2)
-					m[posx][posy-1]=m[posx][posy]=-2;
-				else if(m[posx][posy+1]>=0&&m[posx][posy]==-2)
-					m[posx][posy+1]=m[posx][posy+2]=-2;
-				else if(m[posx][posy]==-2 &&m[posx][posy+1]==-2 )
-					funciona = false;
+				if(data.get(x.charAt(0))==null)
+					data.put(x.charAt(0),new ArrayList<No>());
+				No p = new No();
+				p.num = posy;
+				data.get(x.charAt(0)).add(p);
 			}
+			
+			boolean funciona = true;
+			
+			for(Character c : data.keySet()){
+				Collections.sort(data.get(c),comparator);
+				ArrayList<No> info = data.get(c);
+				for(int i=0;i<info.size();++i){
+					No d = info.get(i);
+					if(d.posi=='-')
+					{
+						for(int j=i+1;j<info.size();++j){
+							No e = info.get(j);
+							if(e.posi=='\0')
+								e.posi='$';
+							else funciona = false;
+						}
+						for(int j=i-1;j>=0;--j)
+						{
+							No e = info.get(j);
+							if(e.posi=='\0')
+								e.posi='$';
+							else 
+								funciona = false;
+						}
+					}
+					else if(d.posi=='+')
+					{
+						for(int j=i+1;j<info.size();++j){
+							No e = info.get(j);
+							if(e.posi=='\0')
+								e.posi='%';
+							else break;
+						}
+						for(int j=i-1;j>=0;--j)
+						{
+							No e = info.get(j);
+							if(e.posi=='\0')
+								e.posi='%';
+							else break;
+						}
+					}
+					
+					
+				}
+				for(int i=0;i<info.size();++i){
+					No d = info.get(i);
+					if(d.posi=='\0'){ System.out.println("d: " + d);funciona=false;}
+				}
+				
+				System.out.println(c +" - " + data.get(c));
+			}
+			
+		
 			
 			sb.append((funciona)?"YES\n":"NO\n");
 		}
