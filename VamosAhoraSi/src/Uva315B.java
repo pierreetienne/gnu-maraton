dc9250b4cb57b76b0ecabec301d7df37d5d883a7
PierreEtienne <etienne.pradere@gmail.com>
2013-03-05 21:22:12 -0500

    Iterativo articularPoint

--- VamosAhoraSi/src/Uva315B.java
+++ VamosAhoraSi/src/Uva315B.java
@@ -1,14 +1,18 @@
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 
+public class Uva315B {
 
-public class Uva315 {
 	static ArrayList<Integer>[] lAdy;
+	static int[] dfs_low, dfs_num, dfs_parent;
+	static boolean[] articulation_vertex;
+	static int cont =0, dfs_root=0, root_Children=0;
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(in.readLine());
@@ -35,7 +39,32 @@ public class Uva315 {
 				if(dfs_num[i]==-1){
 					dfs_root=i;
 					root_Children=0;
-					articulatioPointAndBridge(i);
+					Stack<Integer> p = new Stack<Integer>();
+					p.add(i);
+					while(!p.isEmpty()){
+						int u = p.pop();
+						dfs_low[u]= dfs_num[u] =cont++;
+						for(int j=0;j<lAdy[u].size();++j){
+							int v = lAdy[u].get(j);
+							if(dfs_num[v]==-1){
+								dfs_parent[v]=u;
+								if(u==dfs_root)root_Children++;
+								
+								p.add(v);
+							}
+							else if(v != dfs_parent[u])
+								dfs_low[u]=Math.min(dfs_low[u], dfs_num[v]);
+							else 
+							{
+								if(dfs_low[v]>=dfs_num[u])
+									articulation_vertex[u]=true;
+								if(dfs_low[v]>dfs_num[u]){
+									//Hay bridge
+								}
+								dfs_low[u] = Math.min(dfs_low[u], dfs_low[v]);
+							}
+						}
+					}
 					articulation_vertex[dfs_root]=(root_Children>1);
 				}
 			}
@@ -47,11 +76,7 @@ public class Uva315 {
 		}
 	}
 	
-	static int[] dfs_low, dfs_num, dfs_parent;
-	
-	static boolean[] articulation_vertex;
-	
-	static int cont =0, dfs_root=0, root_Children=0;
+
 	
 	static void articulatioPointAndBridge(int u){
 		dfs_low[u]= dfs_num[u] =cont++;
@@ -74,5 +99,6 @@ public class Uva315 {
 				dfs_low[u]=Math.min(dfs_low[u], dfs_num[v]);
 		}
 	}
+
 
 }
