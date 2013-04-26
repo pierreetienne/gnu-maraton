dab59e19c7cac820d9ecb4ece5ca4706c8727b8e
Pierre Etienne <pierre.pradere@helisa.com>
2013-04-26 14:02:56 -0500

    asd

--- VamosAhoraSi/src/Uva562.java
+++ VamosAhoraSi/src/Uva562.java
@@ -8,28 +8,36 @@ public class Uva562 {
 
 	static int[] m;
 
+	static int[][][] memo;
 
 	static int f(int pos , int l, int x){
-		if(l == x && l+x>0)
-			return 0;
-		else if(pos+1<m.length)
-			return Math.min(f(pos+1, l+m[pos],x),f(pos+1, l, x+m[pos]));
+		if(memo[pos][l][x]!=-1)
+			return memo[pos][l][x];
+		if(pos<m.length)
+			return memo[pos][l][x]= Math.min(f(pos+1, l+m[pos],x),f(pos+1, l, x+m[pos]));
 		else
-			return Math.abs(l-x);
+			return memo[pos][l][x]=Math.abs(l-x);
 	}
 
 
 
 	public static void main(String[] args) throws Exception{
+		int val = 0;
+		for(int i=1;i<=500;++i)
+			val+=i;
+		System.out.println(val);
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(bf.readLine());
+		memo = new int[101][501][501];
 		while(N-->0){
+			for(int[][] x: memo)
+				for(int[] y : x)
+					Arrays.fill(y, -1);
 			int C = Integer.parseInt(bf.readLine());
 			StringTokenizer st = new StringTokenizer(bf.readLine());
 			m = new int[C];
-			int suma = 0 ;
 			for(int i=0;i<C;++i)
-				suma += m[i]=Integer.parseInt(st.nextToken());
+				m[i]=Integer.parseInt(st.nextToken());
 			Arrays.sort(m);
 			int min = f(0, 0, 0);
 
