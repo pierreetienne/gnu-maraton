51dd30ba04fcb9a19477499cc4d122ebf5d96421
Pierre <Pierre@toshiba>
2015-09-01 20:45:06 -0500

    asdfasdfasd

--- VamosAhoraSi/src/CF552A.java
+++ VamosAhoraSi/src/CF552A.java
@@ -1,6 +1,6 @@
-import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.Arrays;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 
 public class CF552A {
@@ -8,15 +8,21 @@ public class CF552A {
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		for(String ln;(ln=in.readLine())!=null;){
-			int[][] m = new int[100][100];
-			for(int i=0,p=100;i<100;++i,--p){
-				for(int j=0;j<100;++j){
-					m[i][j]=p-(j+1);
+			int n = Integer.parseInt(ln);
+			int sum = 0;
+			for(int i=0;i<n;++i){
+				StringTokenizer st = new StringTokenizer(in.readLine());
+				int x1 = Integer.parseInt(st.nextToken());
+				int y1 = Integer.parseInt(st.nextToken());
+				int x2 = Integer.parseInt(st.nextToken());
+				int y2 = Integer.parseInt(st.nextToken());
+				for(int j=x1;j<=x2;++j){
+					for(int p=y1;p<=y2;++p){
+						sum++;
+					}
 				}
-				System.out.println(Arrays.toString(m[i]));
 			}
+			System.out.println(sum);
 		}
-
 	}
-
 }
