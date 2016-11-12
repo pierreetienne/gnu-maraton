805a05b9c5047f3932ace8145fa2d17156dc865c
Pierre Etienne <etienne.pradere@gmail.com>
2016-11-11 20:52:57 -0500

    no se como hacer este ejercicio ya intente con un TreeMap pero T.L en el caso 33 esta ultima implementacion T.L por el remove de PriorityQueue que es O(n) , ya intente con la estructura Dequeue pero W.A puede ser que no la estuviera manejando bien voy a volver a internar esa aprox.

--- VamosAhoraSi/src/EdxWeek2C.java
+++ VamosAhoraSi/src/EdxWeek2C.java
@@ -1,39 +1,39 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Comparator;
+import java.io.PrintStream;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class EdxWeek2C {
-
-	static class QueueCustom {
-		int min;
-		Node head;
-	}
-	
-	static class Node{
-		int value;
-		Node next;
-		Node prev;
-	}
 	
 	public static void main(String[] args)throws Exception  {
+		System.setIn(new FileInputStream("queuemin.in"));
+//		System.setIn(new FileInputStream("out.out"));
+		System.setOut(new PrintStream("queuemin.out"));
 		BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
 		for(String ln;(ln=in.readLine())!=null;){
 			int N=Integer.parseInt(ln);
-			QueueCustom q = new QueueCustom();
+			Queue<Integer> c=new LinkedList<>();
 			StringBuilder sb=new StringBuilder();
+//			Deque<Integer> dq=new LinkedList<>();
+			
+			PriorityQueue<Integer> pq=new PriorityQueue<>();
 			for(int i=0;i<N;++i){
 				ln=in.readLine();
 				StringTokenizer st=new StringTokenizer(ln);
 				String token=st.nextToken();
 				if(token.equals("+")){
-					Node n=new Node();
-					n.value=Integer.parseInt(st.nextToken()); 
+					int val=Integer.parseInt(st.nextToken());
+					pq.add(val);
+					c.add(val);
 				}else if(token.equals("-")){
+					Integer val=c.poll();
+					pq.remove(val);
 				}else{
+					sb.append(pq.peek()).append("\n");
 				}
 			}
 			System.out.print(new String(sb));
